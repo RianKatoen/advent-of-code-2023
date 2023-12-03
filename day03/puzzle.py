dbf9e873e855d0849a7a96581c46246957c19726
Rian Katoen <riankatoen@gmail.com>
2023-12-03 04:42:13 -0500

    day01 - part2

--- day03/puzzle.py
+++ day03/puzzle.py
@@ -55,63 +55,57 @@ def create_info(matrix: list[list[int]]) -> tuple[int, int, dict[tuple[int, int]
 
     return(n_rows, n_columns, symbols, numbers)
 
-def scan_row(col: int, numbers: dict[tuple[int, int], int]) -> dict[tuple[int, int], int]:
+def scan_row(col: int, numbers: dict[tuple[int, int], int], pop_entries: bool = True) -> dict[tuple[int, int], int]:
     valid_numbers: dict[tuple[int, int], int] = {} 
     for (col_start, col_end), number in numbers.items():
-        if number == 617:
-            print((col, (col_start, col_end), number))
-            print(col_start - 1 <= col)
-            print(col_end + 1 >= col)
-            print("")
-
         if col_start - 1 <= col and col_end + 1 >= col:
             valid_numbers[(col_start, col_end)] = number
     
     # clear from look up list for faster lookups later on
-    for pos in valid_numbers:
-       numbers.pop(pos)
+    if pop_entries:
+        for pos in valid_numbers:
+            numbers.pop(pos)
     
     return valid_numbers
 
-
-def part1(file: str) -> int:
-    n_rows, n_columns, symbols, numbers = create_info(input(file))
-    
-    for row in symbols.items():
-        print(row)    
-
-    print("")
-    for number in numbers:
-        print(number.items())
-
-    print("")
-    
-    print("Symbols:", len(symbols), "; Numbers:", len(numbers))
-
+def find_parts(n_rows: int, n_columns: int, symbols: dict[tuple[int, int], str], numbers: list[dict[tuple[int, int], int]], pop_entries: bool = True) -> list[dict[tuple[int, int], int]]:
     legit_numbers: list[dict[tuple[int, int], int]] = []
     for n_row in range(n_rows):
         legit_numbers.append({})
 
     for (row, col) in symbols:
-        legit_numbers[row].update(scan_row(col, numbers[row]))
+        legit_numbers[row].update(scan_row(col, numbers[row], pop_entries))
         if row > 0:
-            legit_numbers[row - 1].update(scan_row(col, numbers[row - 1]))
+            legit_numbers[row - 1].update(scan_row(col, numbers[row - 1], pop_entries))
         if row < n_rows:
-            legit_numbers[row + 1].update(scan_row(col, numbers[row + 1]))
-
-    print("")
-    for number in legit_numbers:
-        print(number.items())
+            legit_numbers[row + 1].update(scan_row(col, numbers[row + 1], pop_entries))
     
-    print("")
-    for number in numbers:
-        print(number.items())
+    return legit_numbers
 
-    for number in legit_numbers:
-        for x in number.values():
-            print(x)
-
+def part1(file: str) -> int:
+    legit_numbers = find_parts(*create_info(input(file)))
     return sum([sum(number.values()) for number in legit_numbers])
+
+def part2(file: str) -> int:
+    n_rows, n_columns, symbols, numbers = create_info(input(file))
+
+    gear_ratios = 0
+    gears = {pos:symbol for pos, symbol in symbols.items() if symbol == "*"}
+    for (pos, gear) in gears.items():
+        parts = [[val for val in part.values() if val > 0] for part in find_parts(n_rows, n_columns, {pos: gear}, numbers, False)]
+
+        legit_numbers = []
+        for row in parts:
+            for val in row:
+                legit_numbers.append(val)
+
+        if len(legit_numbers) == 2:
+            gear_ratios += legit_numbers[0] * legit_numbers[1]
+
+    return gear_ratios
 
 print("example 1:", part1("example.txt"))
 print("part 1:", part1("input.txt"))
+
+print("example 2:", part2("example.txt"))
+print("part 2:", part2("input.txt"))
