7ddbe94287d0af1747e4b6c4c0df84b3427e4676
Rian Katoen <riankatoen@gmail.com>
2023-12-03 04:54:54 -0500

    day03 - refactor

--- day03/puzzle.py
+++ day03/puzzle.py
@@ -1,23 +1,11 @@
 import re
 
-def char_to_int(val: str) -> int:
-    pattern = re.compile(r'\d{1}')
-    if pattern.search(val):
-        return int(val)
-    elif val == '.':
-        return -2
-    else:
-        return -1
+def input(file: str) -> list[list[str]]:
+    return [[char for char in line.strip()] for line in open(file, "r")]
 
-# 0-9 is value
-# -1 is symbol
-# -2 is dot
-def input(file: str) -> list[list[int]]:
-    lines = [[char for char in line.strip()] for line in open(file, "r")]
-    return lines
-
-
+# create two list containing the dictionary of unique symbols and a row-by-row list of number interval.
 def create_info(matrix: list[list[int]]) -> tuple[int, int, dict[tuple[int, int], str], list[dict[tuple[int, int], int]]]:
+    pattern = re.compile(r'\d{1}')
     symbols={}
     numbers=[]
 
@@ -32,47 +20,44 @@ def create_info(matrix: list[list[int]]) -> tuple[int, int, dict[tuple[int, int]
         j = 0
         while j < n_columns:
             char=row[j]
-            val=char_to_int(char)
 
             #number
-            if val > 0:
+            if pattern.search(char):
                 number=""
                 k = j
-                while k < n_columns and char_to_int(row[k]) >= 0:
+                while k < n_columns and pattern.search(row[k]):
                     number += row[k]
                     k += 1
                 row_numbers[(j, k - 1)] = int(number)
                 j = k
-            #symbol
-            elif val == -1:
-                symbols[(i, j)] = char
-                j += 1
+            #symbol or number
             else:
+                if char != '.':
+                    symbols[(i, j)] = char
                 j += 1
         
         numbers.append(row_numbers)
         i += 1
 
-    return(n_rows, n_columns, symbols, numbers)
+    return(symbols, numbers)
 
 def scan_row(col: int, numbers: dict[tuple[int, int], int], pop_entries: bool = True) -> dict[tuple[int, int], int]:
     valid_numbers: dict[tuple[int, int], int] = {} 
     for (col_start, col_end), number in numbers.items():
+        # substract 1 and add 1 to account for diagonals.
         if col_start - 1 <= col and col_end + 1 >= col:
             valid_numbers[(col_start, col_end)] = number
     
-    # clear from look up list for faster lookups later on
+    # Optionally clear from look up list for faster lookups later on
     if pop_entries:
         for pos in valid_numbers:
             numbers.pop(pos)
     
     return valid_numbers
 
-def find_parts(n_rows: int, n_columns: int, symbols: dict[tuple[int, int], str], numbers: list[dict[tuple[int, int], int]], pop_entries: bool = True) -> list[dict[tuple[int, int], int]]:
-    legit_numbers: list[dict[tuple[int, int], int]] = []
-    for n_row in range(n_rows):
-        legit_numbers.append({})
-
+def find_parts(symbols: dict[tuple[int, int], str], numbers: list[dict[tuple[int, int], int]], pop_entries: bool = True) -> list[dict[tuple[int, int], int]]:
+    n_rows=len(numbers)
+    legit_numbers: list[dict[tuple[int, int], int]] = [{} for _ in range(n_rows)]
     for (row, col) in symbols:
         legit_numbers[row].update(scan_row(col, numbers[row], pop_entries))
         if row > 0:
@@ -87,16 +72,15 @@ def part1(file: str) -> int:
     return sum([sum(number.values()) for number in legit_numbers])
 
 def part2(file: str) -> int:
-    n_rows, n_columns, symbols, numbers = create_info(input(file))
+    symbols, numbers = create_info(input(file))
 
     gear_ratios = 0
-    gears = {pos:symbol for pos, symbol in symbols.items() if symbol == "*"}
+    gears = { pos:symbol for pos, symbol in symbols.items() if symbol == "*" }
     for (pos, gear) in gears.items():
-        parts = [[val for val in part.values() if val > 0] for part in find_parts(n_rows, n_columns, {pos: gear}, numbers, False)]
-
+        # this feels stupid but I don't understand python well enough to use proper syntax
         legit_numbers = []
-        for row in parts:
-            for val in row:
+        for part in find_parts({pos: gear}, numbers, False):
+            for val in part.values():
                 legit_numbers.append(val)
 
         if len(legit_numbers) == 2:
