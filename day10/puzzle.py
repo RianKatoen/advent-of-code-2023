f2003a9112ee3c90788f7908388bf6b21fbd4dae
Rian Katoen <riankatoen@gmail.com>
2023-12-10 04:41:41 -0500

    day10 - part2 WIP

--- day10/puzzle.py
+++ day10/puzzle.py
@@ -92,8 +92,6 @@ def move(start: tuple[int, int], dest: tuple[int, int], pipes: list[str], raise_
             else:
                 return (-1, -1)
 
-#def traverse(start: tuple[int, int], pipes: list[str], traversed: dict[tuple]):
-
 def part1(file: str, verbosity: int = 0) -> int:
     pipes = input(file)
     start = find_starting_position(pipes)
@@ -130,5 +128,149 @@ def part1(file: str, verbosity: int = 0) -> int:
     return max([n_moves for n_moves in max_moves.values()])
 
 for i in range(1, 3):
-    print(f"example {i}:", part1(f"example{i}.txt", 1))
-print("part 1:", part1(f"input.txt", 1))
+    print(f"example {i}:", part1(f"example{i}.txt", 0))
+print("part 1:", part1(f"input.txt", 0))
+print("")
+
+def turn_right(start: tuple[int, int], dest: tuple[int, int], pipes: list[str]) -> list[tuple[int, int]]:
+    i1, j1 = start
+    i2, j2 = dest
+    
+    if pipes[i2][j2] == '-':
+        if j2 > j1:
+            return [(i2 + 1, j2)]
+        else:
+            return [(i2 - 1, j2)]
+        
+    elif pipes[i2][j2] == '|':
+        if i2 > i1:
+            return [(i2, j2 - 1)]
+        else:
+            return [(i2, j2 + 1)]
+        
+    elif pipes[i2][j2] == 'L' and i1 < i2:
+        return [(i2, j2 - 1), (i2 + 1, j2)]
+    elif pipes[i2][j2] == 'J' and j1 < j2:
+        return [(i2 + 1, j2), (i2, j2 + 1)]
+    elif pipes[i2][j2] == '7' and i2 < i1:
+        return [(i2 - 1, j2), (i2, j2 + 1)]
+    elif pipes[i2][j2] == 'F' and j2 < j1: 
+        return [(i2 - 1, j2), (i2, j2 - 1)]
+    else:
+        return []
+
+def turn_left(start: tuple[int, int], dest: tuple[int, int], pipes: list[str]) -> list[tuple[int, int]]:
+    i1, j1 = start
+    i2, j2 = dest
+    
+    i = i2 - i1
+    j = j2 - j1
+
+    if pipes[i2][j2] == '-' or pipes[i2][j2] == '|':
+        if i == 0 and j == 1:
+            return [(i2 - 1, j2)]
+        elif i == 1 and j == 0:
+            return [(i2, j2 + 1)]
+        elif i == 0 and j == -1:
+            return [(i2 + 1, j2)]
+        elif i == -1 and j == 0:
+            return [(i2, j2 - 1)]
+    else:
+        return []
+
+def part2(file: str, verbosity: int = 0) -> int:
+    print("")
+    print(traverse(file, lambda s, d, p: turn_right(s, d, p), verbosity))
+    #print(traverse(file, lambda s, d, p: turn_left(s, d, p), verbosity))
+    return 0
+
+def get_externals(pipes: tuple[list[str]]) -> set[tuple[int, int]]:
+    n_rows, n_cols = len(pipes), len(pipes[0])
+
+    externals = set()
+    for i in range(0, n_rows):
+        for j in range(0, n_cols):
+            if pipes[i][j] != '.':
+                break
+            externals.add((i, j))
+
+    for i in range(0, n_rows):
+        for j in range(0, n_cols)[::-1]:
+            if pipes[i][j] != '.':
+                break
+            externals.add((i, j))
+
+    for j in range(0, n_cols):
+        for i in range(0, n_rows):
+            if pipes[i][j] != '.':
+                break
+            externals.add((i, j))
+
+    for j in range(0, n_cols)[::-1]:
+        for i in range(0, n_rows):
+            if pipes[i][j] != '.':
+                break
+            externals.add((i, j))
+
+    return externals
+
+def traverse(file: str, turner_fn, verbosity: int = 0) -> int:
+    pipes = input(file)
+    externals = get_externals(pipes)
+    start = find_starting_position(pipes)
+    destinations = find_start(start, pipes)
+
+    n_rows, n_cols = len(pipes), len(pipes[0])
+    if verbosity > 0:
+        print(file, " [starting position] ", start)
+        print(file, " [destinations] ", destinations)
+    
+    all_tiles = []
+    for destination in destinations:
+        path = set()
+        path.add(start)
+        tiles = set()
+    
+        i1, j1 = start
+        i2, j2 = destination
+        if verbosity > 1:
+            print("")
+            print(file, " [right_track] ", pipes[start[0]][start[1]], start, "->", pipes[destination[0]][destination[1]], destination)
+
+        while destination != start:
+            path.add(destination)
+            i2, j2 = destination
+            turns = turner_fn((i1, j1), (i2, j2), pipes)
+            for i3, j3 in turns:
+                if (i3, j3) in externals or i3 < 0 or j3 < 0:
+                    tiles = set()
+                elif i3 >= 0 and j3 >= 0 and i3 < n_rows and j3 < n_cols:
+                    if verbosity > 1:
+                        print(file, " [turn] ", (i3, j3))
+                    tiles.add((i3, j3))
+
+            destination = move((i1, j1), (i2, j2), pipes)
+
+            i1, j1 = i2, j2
+            if verbosity > 1:
+                print(file, " [next_destination] ", pipes[i1][j1], (i1, j1), "->", pipes[destination[0]][destination[1]], destination)
+
+        for tile in [t for t in tiles]:
+            if tile in path:
+                tiles.remove(tile)
+
+        if verbosity > 1:
+            for tile in [t for t in tiles]:
+                print(file, " [tile] ", tile)
+
+        all_tiles.append(tiles)
+
+    return max([len(tiles) for tiles in all_tiles])
+    
+print("example 1:", part2(f"example1.txt", 0))
+print("example 3:", part2(f"example3.txt", 0))
+print("example 4:", part2(f"example4.txt", 0))
+print("example 5:", part2(f"example5.txt", 0))
+print("example 6:", part2(f"example6.txt", 0))
+print("example 7:", part2(f"example7.txt", 0))
+print("part 2:", part2(f"input.txt", 0))
