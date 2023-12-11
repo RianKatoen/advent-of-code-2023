965259be6c1e6cfaa6392c41270edd32c262af43
Rian Katoen <riankatoen@gmail.com>
2023-12-11 15:15:52 -0500

    day11 - part2

--- day11/puzzle.py
+++ day11/puzzle.py
@@ -10,25 +10,31 @@ def inflate_map(map: list[list[str]]) -> list[list[str]]:
     for row in map:
         new_map.append(row)
         if all(r == '.' for r in row):
-            new_map.append(row)
+            new_map.append('0' * len(row))
     
     i = 0
     while i < len(new_map[0]):
-        if all(r[i] == '.' for r in new_map):
-            i += 1
+        if all(r[i] == '.' or r[i] == '0' for r in new_map):
             for r, j in zip(new_map, range(len(new_map))):
-                new_map[j] = r[0:i] + '.' + r[i:]
+                new_map[j] = r[0:i] + '0' + r[i:]
+            i += 1
         i += 1
-    
+
     return new_map
 
-def find_galaxies(map: list[list[str]]) -> set[tuple[int, int]]:
+def find_galaxies(map: list[list[str]], inflation_factor: int = 2) -> set[tuple[int, int]]:
     galaxies: set[tuple[int, int]] = set()
 
+    row = 0
     for i in range(len(map)):
+        col = 0
         for j in range(len(map[0])):
             if map[i][j] == '#':
-                galaxies.add((i, j))
+                galaxies.add((row, col))
+            
+            col += (inflation_factor - 1) if map[i][j] == '0' else 1
+        
+        row += (inflation_factor - 1) if map[i][0] == '0' else 1
 
     return galaxies
     
@@ -41,13 +47,18 @@ def calculate_distance(galaxy1: tuple[int, int], galaxy2: tuple[int, int]) -> in
     i2, j2 = galaxy2
     return abs(i1 - i2) + abs(j1 - j2)
 
-def part1(file: str) -> int:
+def sum_of_distances(file: str, inflation_factor: int = 2) -> int:
     map = input(file)
     map = inflate_map(map)
-    galaxies = find_galaxies(map)
+    galaxies = find_galaxies(map, inflation_factor)
     pairs = find_pairs(galaxies)
 
     return sum(calculate_distance(g1, g2) for g1, g2 in pairs)
 
-print("example 1:", part1("example1.txt"))
-print("part 1:", part1("input.txt"))
+print("example 1:", sum_of_distances("example1.txt"))
+print("part 1:", sum_of_distances("input.txt"))
+
+print("")
+print("example 2:", sum_of_distances("example1.txt", 10))
+print("example 2:", sum_of_distances("example1.txt", 100))
+print("part 2:", sum_of_distances("input.txt", 1000000))
